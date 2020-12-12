5eb96eaef27eebb750096c576aff58d4d06c138e
Satont <satontworldwide@gmail.com>
2020-12-13 00:15:42 +0300

    upd

--- src/client/pages/Players/list.vue
+++ src/client/pages/Players/list.vue
@@ -3,44 +3,49 @@
     :headers="headers"
     :items="players"
     :items-per-page="20"
+    :page.sync="page"
     class="elevation-1"
   >
     <template v-slot:[`item.teams`]="{ item }">
-      <v-slide-group multiple show-arrows>
-        <v-slide-item v-for="team in item.teams" :key="team.id">
-          <v-btn
-            class="mx-2"
-            small
-            depressed
-            rounded
-            @click="$router.push({
-              name: 'TeamsEdit',
-              params: { id: team.id }
-            })"
-          >
-            {{ team.name }}
-          </v-btn>
-        </v-slide-item>
-      </v-slide-group>
+      <v-sheet max-width="300">
+        <v-slide-group multiple show-arrows>
+          <v-slide-item v-for="team in item.teams" :key="team.id">
+            <v-btn
+              class="mx-2"
+              small
+              depressed
+              rounded
+              @click="$router.push({
+                name: 'TeamsEdit',
+                params: { id: team.id }
+              })"
+            >
+              {{ team.name }}
+            </v-btn>
+          </v-slide-item>
+        </v-slide-group>
+      </v-sheet>
     </template>
 
      <template v-slot:[`item.teams.tournaments`]="{ item }">
-      <v-slide-group multiple show-arrows>
-        <v-slide-item v-for="tournament in getTournamentsFromItem(item)" :key="tournament.id">
-          <v-btn
-            class="mx-2"
-            small
-            depressed
-            rounded
-             @click="$router.push({
-              name: 'TournamentsEdit',
-              params: { id: tournament.id }
-            })"
-          >
-          {{ tournament.name }}
-          </v-btn>
-        </v-slide-item>
-      </v-slide-group>
+      <v-sheet max-width="300">
+        <v-slide-group multiple show-arrows>
+          <v-slide-item v-for="tournament in getTournamentsFromItem(item)" :key="tournament.id">
+            <v-btn
+              class="mx-2"
+              small
+              depressed
+              rounded
+              @click="$router.push({
+                name: 'TournamentsEdit',
+                params: { id: tournament.id }
+              })"
+            >
+            {{ tournament.name }}
+            </v-btn>
+          </v-slide-item>
+        </v-slide-group>
+      </v-sheet>
     </template>
 
      <template v-slot:[`item.actions`]="{ item }">
@@ -66,10 +71,10 @@ import { mdiPencil } from '@mdi/js'
 export default Vue.extend({
   data: () => ({
     headers: [
-      { text: 'Discord', value: 'discord.tag' },
-      { text: 'Activision ID', value: 'activision' },
-      { text: 'Состоит в команде', value: 'teams' },
-      { text: 'Участник турнира', value: 'teams.tournaments' },
+      { text: 'Discord', value: 'discord.tag', sortable: false },
+      { text: 'Activision ID', value: 'activision', sortable: false },
+      { text: 'Состоит в команде', value: 'teams', sortable: false },
+      { text: 'Участник турнира', value: 'teams.tournaments', sortable: false },
       { text: 'Actions', value: 'actions', sortable: false },
     ],
     players: [],
