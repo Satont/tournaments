6689b404697b4e9b0fbf83d50eeabe696861704d
Satont <satontworldwide@gmail.com>
2020-12-17 07:12:09 +0300

    upd

--- src/client/pages/Players/edit.vue
+++ src/client/pages/Players/edit.vue
@@ -1,11 +1,11 @@
 <template>
 <div>
-  <h2>Игрок {{ form.discord.tag }}</h2>
-  <v-form v-model="valid">
+  <h2>Игрок {{ form.tag }}</h2>
+  <v-form>
     <v-container>
       <v-row>
         <v-col cols="12" md="2">
-          <v-text-field v-model="form.discord.tag" label="DISCORD" required />
+          <v-text-field v-model="form.tag" label="DISCORD" required />
         </v-col>
 
         <v-col cols="12" md="2">
@@ -13,25 +13,7 @@
         </v-col>
 
         <v-col cols="12" md="4">
-         <v-autocomplete
-            v-model="items"
-            :items="chips"
-            chips
-            small-chips
-            label="РОЛЬ В DICORD (KDA)"
-            multiple
-          ></v-autocomplete>
-        </v-col>
-
-         <v-col cols="12" md="4">
-         <v-autocomplete
-            v-model="items"
-            :items="chips"
-            chips
-            small-chips
-            label="СОСТОИТ В КОМАНДЕ"
-            multiple
-          ></v-autocomplete>
+          <v-autocomplete v-model="form.roles" :items="rolesForSelection" chips small-chips label="РОЛЬ В DICORD (KDA)" multiple></v-autocomplete>
         </v-col>
       </v-row>
     </v-container>
@@ -42,27 +24,39 @@
 <script lang="ts">
 import Vue from 'vue'
 import axios from 'axios'
+import { Role } from 'discord.js'
 
 export default Vue.extend({
   data: () => ({
     form: {
-      discord: {
-        tag: '',
-      },
+      tag: '',
+      activision: '',
+      roles: [],
     },
-    valid: false,
-    chips: ['test1', 'test2'],
-    items: [],
   }),
   async mounted() {
     const { data } = await axios.get(`/api/players/${this.$route.params.id}`)
-    this.form = data
+
+    this.form.activision = data.activision
+    this.form.tag = data.discord.tag
+    this.form.roles = data.discord.roles.map(role => role.id)
   },
   methods: {
     remove (item) {
-      this.chips.splice(this.chips.indexOf(item), 1)
-      this.chips = [...this.chips]
+      this.chips = this.chips.filter(c => c !== item)
     },
   },
+  computed: {
+    rolesForSelection() {
+      const list = this.$store.state.roles
+        .filter(r => this.$store.state.settings.roles.some(rr => rr.id === r.id))
+        .map((role: Role) => ({
+          text: role.name,
+          value: role.id,
+        }))
+
+      return list
+    },
+  }
 })
 </script>
